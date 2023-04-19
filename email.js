d01986de5eac1022780ec34419a0d8f89575bf94
Martin Wassner <wassner.martin@gmail.com>
2023-04-19 18:36:39 -0300

    confirmation button works

--- email.js
+++ email.js
@@ -1,7 +1,10 @@
+
+
 const btn = document.getElementById('button');
 
 document.getElementById('form')
  .addEventListener('submit', function(event) {
+
    event.preventDefault();
 
    btn.value = 'Sending...';
@@ -11,10 +14,15 @@ document.getElementById('form')
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
+      btn.value = 'Email sent!';
+      btn.classList.add('success');
+      setTimeout(() => {
+        btn.classList.remove('success');
+        btn.value = 'Send Email'
+      }, 2000);
     }, (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
     });
 });
+
